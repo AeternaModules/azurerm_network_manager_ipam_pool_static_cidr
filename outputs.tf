707d02c642a77cdc75fc8afdef8712ab8c99f9ee
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:20 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,17 @@
-output "network_manager_ipam_pool_static_cidrs" {
-  description = "All network_manager_ipam_pool_static_cidr resources"
-  value       = azurerm_network_manager_ipam_pool_static_cidr.network_manager_ipam_pool_static_cidrs
-}
 output "network_manager_ipam_pool_static_cidrs_address_prefixes" {
-  description = "List of address_prefixes values across all network_manager_ipam_pool_static_cidrs"
-  value       = [for k, v in azurerm_network_manager_ipam_pool_static_cidr.network_manager_ipam_pool_static_cidrs : v.address_prefixes]
+  description = "Map of address_prefixes values across all network_manager_ipam_pool_static_cidrs, keyed the same as var.network_manager_ipam_pool_static_cidrs"
+  value       = { for k, v in azurerm_network_manager_ipam_pool_static_cidr.network_manager_ipam_pool_static_cidrs : k => v.address_prefixes }
 }
 output "network_manager_ipam_pool_static_cidrs_ipam_pool_id" {
-  description = "List of ipam_pool_id values across all network_manager_ipam_pool_static_cidrs"
-  value       = [for k, v in azurerm_network_manager_ipam_pool_static_cidr.network_manager_ipam_pool_static_cidrs : v.ipam_pool_id]
+  description = "Map of ipam_pool_id values across all network_manager_ipam_pool_static_cidrs, keyed the same as var.network_manager_ipam_pool_static_cidrs"
+  value       = { for k, v in azurerm_network_manager_ipam_pool_static_cidr.network_manager_ipam_pool_static_cidrs : k => v.ipam_pool_id }
 }
 output "network_manager_ipam_pool_static_cidrs_name" {
-  description = "List of name values across all network_manager_ipam_pool_static_cidrs"
-  value       = [for k, v in azurerm_network_manager_ipam_pool_static_cidr.network_manager_ipam_pool_static_cidrs : v.name]
+  description = "Map of name values across all network_manager_ipam_pool_static_cidrs, keyed the same as var.network_manager_ipam_pool_static_cidrs"
+  value       = { for k, v in azurerm_network_manager_ipam_pool_static_cidr.network_manager_ipam_pool_static_cidrs : k => v.name }
 }
 output "network_manager_ipam_pool_static_cidrs_number_of_ip_addresses_to_allocate" {
-  description = "List of number_of_ip_addresses_to_allocate values across all network_manager_ipam_pool_static_cidrs"
-  value       = [for k, v in azurerm_network_manager_ipam_pool_static_cidr.network_manager_ipam_pool_static_cidrs : v.number_of_ip_addresses_to_allocate]
+  description = "Map of number_of_ip_addresses_to_allocate values across all network_manager_ipam_pool_static_cidrs, keyed the same as var.network_manager_ipam_pool_static_cidrs"
+  value       = { for k, v in azurerm_network_manager_ipam_pool_static_cidr.network_manager_ipam_pool_static_cidrs : k => v.number_of_ip_addresses_to_allocate }
 }
 
